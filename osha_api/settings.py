c814fa7c5397f72134f00234ed1ec3dd933a387a
Maricarmen <maricarmencoaquiracuevas@gmail.com>
2025-03-19 18:05:56 -0500

    settings

--- osha_api/settings.py
+++ osha_api/settings.py
@@ -181,7 +181,7 @@ CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-d405.up.railway.app']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny', #IsAuthenticated
+        'rest_framework.permissions.IsAuthenticated', #AllowAny IsAuthenticated
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
@@ -192,34 +192,36 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    
+    #'ROTATE_REFRESH_TOKENS': False,
+    #'BLACKLIST_AFTER_ROTATION': True,
+    #'UPDATE_LAST_LOGIN': False,
+    #'ALGORITHM': 'HS256',
+    
+    #'VERIFYING_KEY': None,
+    #'AUDIENCE': None,
+    #'ISSUER': None,
+    #'JWK_URL': None,
+    #'LEEWAY': 0,
+    
+    'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=6), # Cambiado a 6 hora
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1), # Duración del token de refresco (puedes ajustar esto si es necesario)
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
-    'ALGORITHM': 'HS256',
-    
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-    
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    #'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
     'USER_ID_FIELD': 'usuario',
     'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    #'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
     
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    'AUTH_TOKEN_CLASSES': (
+        'rest_framework_simplejwt.tokens.AccessToken',
+    ),
     
-    'JTI_CLAIM': 'jti',
-    
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    #'TOKEN_TYPE_CLAIM': 'token_type',
+    #'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    #'JTI_CLAIM': 'jti',
+    #'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    #'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    #'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
 DJOSER = {
